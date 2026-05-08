33220e7266a5f75c9e7fac8169b7b727c68038ab
Gabriel Coutinho <125703161+bielsolosos@users.noreply.github.com>
2026-05-08 15:37:58 -0300

    feat: adicionando eslint

--- nx-barrel-demo-replica/libs/shared-ui/src/index.ts
+++ nx-barrel-demo-replica/libs/shared-ui/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./lib/components/button/button.component";
 export * from "./lib/components/card/card.component";
-
 // O ERRO ACONTECE AQUI: As constantes no final fazem com que o AuthService
 // (chamado pelo CardComponent acima) tente acessar THEME_CONFIG antes dele existir.
 export * from "./lib/constants/theme.constants";
